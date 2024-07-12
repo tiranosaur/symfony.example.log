acfde099e7e774d9864f6df162cc657c1742ed24
tiranosaur <tiranosaur88@gmail.com>
2024-07-12 23:21:43 +0300

    feat: added MainService

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\MainService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main_index')]
-    public function index(): Response
+    public function index(MainService $mainService): Response
     {
         return $this->render('main/index.html.twig', [
             'title' => 'Main Page',
+            'message' => $mainService->getMessage(),
             'data' => [
             ]
         ]);
---
+++ src/Service/MainService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+class MainService
+{
+    public function getMessage(): string
+    {
+        return "Hello from service";
+    }
+}
